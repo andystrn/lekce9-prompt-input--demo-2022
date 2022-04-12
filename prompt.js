858dd0b17ab1e5ce97796a8841f3deb54473266b
karolyim <matej.karolyi@gmail.com>
2022-04-12 18:31:50 +0200

    [FEATURE] Implement prompt

---
+++ prompt.js
@@ -0,0 +1,6 @@
+//vytvoření promptů pro zadání jména + příjmení a věku a naplnění do proměnných
+let celeJmeno = prompt('Zadej jméno a příjmení');
+let vek = prompt('Zadej věk', 18);
+
+// vypsání výsledků pomocí alert()
+alert(`Jmenuješ se ${celeJmeno} a je ti ${vek} let.`);
