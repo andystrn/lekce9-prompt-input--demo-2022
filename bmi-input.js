c1eb52645ef3cdf1c19b1d12d152e0b57bdc65e7
karolyim <matej.karolyi@gmail.com>
2022-04-12 19:02:39 +0200

    [ENHANCEMENT] Propagate BMI result to UI

--- bmi-input.js
+++ bmi-input.js
@@ -5,7 +5,12 @@ function vypocitejBMI() {
 
     let BMI = Math.round(vahaKilogramy / Math.pow(vyskaMetry, 2));
 
-    console.log(BMI);
+    vypisBMI(BMI);
+}
+
+// funkce pro zapsání BMI do UI
+function vypisBMI(BMI) {
+    document.querySelector('#vysledek > span').innerHTML = BMI;
 }
 
 document.querySelectorAll('input').forEach((element) => {
