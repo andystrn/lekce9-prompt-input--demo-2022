b71f1a3746d8db7c984640453c91e011703b48f0
Andrea Strnadlova <an.strnadlova@gmail.com>
2022-04-22 12:20:39 +0200

    [REFACTOR] Separate BMI calculation and printing

--- bmi-input.js
+++ bmi-input.js
@@ -3,9 +3,7 @@ function vypocitejBMI() {
     let vyskaMetry = parseFloat(document.querySelector('input[name="vyska"]').value / 100);
     let vahaKilogramy = parseFloat(document.querySelector('input[name="vaha"]').value);
 
-    let BMI = Math.round(vahaKilogramy / Math.pow(vyskaMetry, 2));
-
-    vypisBMI(BMI);
+    return Math.round(vahaKilogramy / Math.pow(vyskaMetry, 2));
 }
 
 //funkce pro zapsání do UI
@@ -13,6 +11,11 @@ function vypisBMI(BMI) {
     document.querySelector('#vysledek > span').innerHTML = BMI;
 }
 
+function vypocitejVypisBMI() {
+    let BMI = vypocitejBMI();
+    vypisBMI(BMI);
+}
+
 document.querySelectorAll('input').forEach((element) => {
-    element.addEventListener('change', vypocitejBMI);
+    element.addEventListener('change', vypocitejVypisBMI);
 });
